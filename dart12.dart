7ef1150bc773b0b6a4ab2363190120ff2d0cf874
Sneha-Uppu <81636785+Sneha-Uppu@users.noreply.github.com>
2022-06-18 19:00:10 +0530

    second commit

--- dart12.dart
+++ dart12.dart
@@ -1,4 +1,4 @@
-//Lambda Expression
+//Lambda Expression and Higher order functions
 
 void main() {
 
@@ -26,6 +26,15 @@ void main() {
 
 	addNumbers(3, 7);
 	print(multiplyFour(10));
+	
+	
+	//Passing Function to Higher-Order Function
+	Function addNumbers = (a, b) => print(a + b);
+	someOtherFunction("Hello", addNumbers);
+	
+	//Receiving Function from Higher-Order Function
+	var myFunc = taskToPerform();
+	print(myFunc(10));
 }
 
 void addMyNumbers(int a, int b)
@@ -33,3 +42,18 @@ void addMyNumbers(int a, int b)
 	var sum = a + b;
 	print(sum);
 }
+
+//Accepts function as parameter
+void someOtherFunction(String message, Function myFunction)       // Higher-Order Function
+{      
+	print(message);
+	myFunction(2, 4); 
+}
+
+// Returns a function
+Function taskToPerform()             // Higher-Order Function
+{      
+	Function multiplyFour = (int number) => number * 4;
+	return multiplyFour;
+}
+
